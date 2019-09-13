0b8c9ef555c0c640cdc0f768523da2e1b5809777
Felipe Modesto <fmodesto@gmail.com>
2019-09-13 01:35:38 -0300

    Adicinando Rest Service

---
+++ MajuApp/src/main/java/com/maju/backend/MyApplication.java
@@ -0,0 +1,13 @@
+package com.maju.backend;
+
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("rest")
+public class MyApplication extends ResourceConfig {
+	
+	 public MyApplication() {
+	        packages("com.maju.backend.controllers");
+	    }
+}
